1634243861f0ffcff7fc8f55a908ec706e92e16b
Aakash <j.akash0303@gmail.com>
2022-12-23 00:34:26 +0545

    First commit

---
+++ client/src/pages/Homepage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "../../src/styles.css"
+function Homepage() {
+  return (
+    <div className="Homepage">
+        <div className="nav">
+            <img src="https://relaxed-monstera-b62a85.netlify.app/assets/logo.62c9c431.svg" alt="logo" />
+            <h3>Crowd Funding</h3>
+        </div>
+        <div className="content">
+            <div className="details">
+                <h1>JHJSHJAHHH</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium officiis perferendis a voluptatum ipsum, aut facilis cum maxime nobis? Officiis!</p>
+                <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos molestias dolores aspernatur perferendis nisi fuga obcaecati blanditiis distinctio, ut delectus, modi beatae eum esse ad labore praesentium sint! In, fuga.</h4>
+            </div>
+            <div className="banner">
+                <div className="create"></div>
+                <div className="donate"></div>
+                <div className="track"></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Homepage
