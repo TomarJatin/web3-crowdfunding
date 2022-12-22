e4e075ea64c930a45eeb3051b147d2f29b24bd7a
Aakash <j.akash0303@gmail.com>
2022-12-23 00:51:35 +0545

    Styles and CSS added

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -9,9 +9,10 @@ function Homepage() {
         </div>
         <div className="content">
             <div className="details">
-                <h1>JHJSHJAHHH</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium officiis perferendis a voluptatum ipsum, aut facilis cum maxime nobis? Officiis!</p>
+                <h1>Crowd Funding</h1>
+                {/* <p>What is <span>Crowd Funding?</span></p> */}
                 <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos molestias dolores aspernatur perferendis nisi fuga obcaecati blanditiis distinctio, ut delectus, modi beatae eum esse ad labore praesentium sint! In, fuga.</h4>
+                <button>Get Started</button>
             </div>
             <div className="banner">
                 <div className="create"></div>
